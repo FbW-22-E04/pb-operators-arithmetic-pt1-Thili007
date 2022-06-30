5109027c823f4db9a721471eac19e48a7d8b10b8
Your Name <tpunchihewage@gmail.com>
2022-06-30 15:48:11 +0200

    exercise 2 added

--- solution.js
+++ solution.js
@@ -1,1 +1,98 @@
 // write your code in this file
+
+// Q1 - !
+const firstNightTicket = 6450;
+const oneTicketPrice = 15;
+
+const totalTickets = firstNightTicket / oneTicketPrice;
+
+console.log(totalTickets);
+
+// Q2 - !
+
+const sylviaIncome = 500;
+const sylviaYearIncome = sylviaIncome * 4 * 12;
+
+console.log(sylviaYearIncome);
+
+// q3 - !
+
+const calPercentage = (17 / 30) % 10;
+console.log(calPercentage);
+
+// Q4 - !
+
+const oneSideLength = 4.75;
+const squarePerimeter = 4.75 * 4;
+
+console.log(squarePerimeter);
+
+// Q5 - !
+
+const perimeterTriangle = 5 + 6 + 7;
+
+console.log("length of Triangle is", perimeterTriangle);
+
+// Q6 - !
+
+const areaOfSquare = 5 * 4;
+
+console.log(areaOfSquare);
+
+// Q7 - !
+
+const rightAngledTriangle = 3 + 4 + 5;
+
+console.log(rightAngledTriangle);
+
+// Q8 - !
+
+const cubeSideLength = 9;
+const cubeVolume = cubeSideLength * 4 * 6;
+
+console.log(cubeVolume);
+
+// Q9 - !
+
+const firstBill = 22.35 + (22.35 % 10);
+
+// console.log(firstBill);
+
+const secondBill = 26.67 + (26.67 % 15);
+
+// console.log(secondBill);
+
+const thirdBill = 35.92 + (35.92 % 20);
+
+console.log(firstBill, secondBill, thirdBill);
+
+// Q10 - !
+
+const noemyWorkedHours = 8 + 6 + 5 + 9 + 8 + 2 + 1 + 8.5 + 7 + 4;
+const averageHoursWoredDay = noemyWorkedHours / 10;
+
+console.log(averageHoursWoredDay);
+
+// Q11 - !
+
+const studentScore = 75 + 70 + 85 + 90 + 100;
+const fiveTestAverage = studentScore;
+
+const sixthaverage = 85 * 6;
+const allAverage = sixthaverage - fiveTestAverage;
+
+console.log("Q11: Score in the sixth test:", allAverage);
+
+// Q12 - !
+
+const firstClassAverage = 80 * 9;
+
+const currentAverage = 78 * 8;
+
+const balanceAverage = firstClassAverage - currentAverage;
+
+console.log(
+  "james needs a minimum of",
+  balanceAverage + "%",
+  "to get an 80% average."
+);
